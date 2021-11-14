a807e9e043b6c21729f594c7159525007f668695
s4goodwin <90581608+s4goodwin@users.noreply.github.com>
2021-11-14 16:52:02 -0500

    rewriting project 3 w/ demo suppliment
    finished TEC

--- src/TaxExemptCustomer.java
+++ src/TaxExemptCustomer.java
@@ -1,2 +1,25 @@
-public class TaxExemptCustomer {
+import java.util.ArrayList;
+
+public class TaxExemptCustomer extends Customer{
+
+    public TaxExemptCustomer(String Name, int id){
+        super(Name, id);
+    }
+    public TaxExemptCustomer(String name){
+        super(name);
+    }
+
+    @Override
+    public double payForOrder(ArrayList<MerchandiseItem> itemsInOrder){
+        var total=0.0;
+        for (var item:itemsInOrder){
+            total+=item.getPrice();
+        }
+        return total;
+    }
+
+    @Override
+    public void arrangeDelivery(){
+        System.out.println("please contact "+getName()+" on the day of delivery");
+    }
 }
