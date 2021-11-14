435d5338652fbde05181fd0e2193533a93d6a9a3
s4goodwin <90581608+s4goodwin@users.noreply.github.com>
2021-11-14 17:02:44 -0500

    rewriting project 3 w/ demo suppliment
    finished ShippingAddress

--- src/ShippingAddress.java
+++ src/ShippingAddress.java
@@ -16,4 +16,10 @@ public class ShippingAddress {
         this.State = State;
         PostalCode = postCode;
     }
+
+    public String toString(){
+        if (AddressLine2.length()>0)
+            return AddressLine1+'\n'+AddressLine2+'\n'+City+", "+State+" "+PostalCode;
+        return AddressLine1+'\n'+City+", "+State+" "+PostalCode;
+    }
 }
