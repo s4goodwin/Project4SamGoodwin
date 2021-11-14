7697f63bd5fcd35a2a394de7f98bdb617ff15d7d
s4goodwin <90581608+s4goodwin@users.noreply.github.com>
2021-11-14 16:29:02 -0500

    rewriting project 3 w/ demo suppliment
    finished RC

--- src/ResidentialCustomer.java
+++ src/ResidentialCustomer.java
@@ -1,2 +1,38 @@
-public class ResidentialCustomer {
+import java.util.ArrayList;
+
+public class ResidentialCustomer extends Customer{
+
+    public ResidentialCustomer(String nomen, int id){
+        super(nomen, id);
+    }
+    public ResidentialCustomer(String name){
+        super(name);
+    }
+
+    public double payForOrder(ArrayList<MerchandiseItem> itemsInCart){
+        var total=0.0;
+        for (var item : itemsInCart){
+            switch (item.getTaxibleType()){
+                case WICFood -> {
+                    total +=item.getPrice();
+                }
+                case GeneralMerchandise -> {
+                    var price=item.getPrice();
+                    var tax=price*0.0625;
+                    total+=price+tax;
+                }
+                case Clothing -> {
+                    var price=item.getPrice();
+                    var tax=0.0;
+                    if(price>175){
+                        var taxiblePrice=price-175;
+                        tax=taxiblePrice*0.0625;
+                    }
+                    total+=price+tax;
+                }
+
+            }
+        }
+        return total;
+    }
 }
