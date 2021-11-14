ed5b56dc916b84fcb55612b9c6bd6011fcc46ded
s4goodwin <90581608+s4goodwin@users.noreply.github.com>
2021-11-14 16:56:37 -0500

    rewriting project 3 w/ demo suppliment
    finished Order

--- src/Order.java
+++ src/Order.java
@@ -1,2 +1,21 @@
+import java.util.ArrayList;
+
 public class Order {
+
+    private ShippingAddress destination;
+    private Customer orderedBy;
+    ArrayList<MerchandiseItem> cartForOrder;
+
+    public Order(ShippingAddress dest, Customer cust, ArrayList<MerchandiseItem> itemsOrdered){
+        destination=dest;
+        orderedBy=cust;
+        cartForOrder=itemsOrdered;
+    }
+
+    public String getDestination(){
+        return destination.toString();
+    }
+    public String getOrderer(){
+        return orderedBy.toString();
+    }
 }
